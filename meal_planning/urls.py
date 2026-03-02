e19bfd63e60b01ff6eb8e67495b4c3d701852c5c
Oleksandra K <oleksandra.kondieieva@gmail.com>
2026-03-01 21:28:39 -0700

    Implemented AI recipe generation: - Integrated Gemini AI model - Created get_recipe_suggestion to handle AI logic - Added url route for /generate_recipes/ - Added recipes.html with <details> tag for instructions - Configured .gitignore to protect sensitive information

--- meal_planning/urls.py
+++ meal_planning/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ingredient_list, add_ingredient
+from .views import ingredient_list, add_ingredient, get_recipe_suggestions
 
 urlpatterns = [
     path("", ingredient_list, name="ingredient-list"),
     path("add/", add_ingredient, name="add-ingredient"),
+    path("generate-recipe/", get_recipe_suggestions, name="generate-recipe")
 ]
--- meal_planning/views.py
+++ meal_planning/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render, redirect
+import google.generativeai as genai
+from google.generativeai.types import GenerationConfig
+from dotenv import load_dotenv
+import json
+import os
+from django.http import JsonResponse
 from .models import Ingredient
 from .forms import IngredientForm
 # Create your views here.
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+genai.configure(api_key=api_key)
+
+model = genai.GenerativeModel(
+    model_name = "gemini-3-flash-preview",
+    system_instruction = "You are a recipe generator. Respond ONLY in valid JSON. No conversational filler.",
+    generation_config=GenerationConfig(response_mime_type="application/json")
+)
 
 def ingredient_list(request):
     ingredients = Ingredient.objects.all()
@@ -17,3 +33,22 @@ def add_ingredient(request):
         form = IngredientForm()
 
     return render(request, "meal_planning/add.html", {"form": form})
+
+def get_recipe_suggestions(request):
+    ingredients = Ingredient.objects.all()
+    ingredients_for_api_call = ", ".join([f"{i.amount} {i.unit} of {i.name}" for i in ingredients])
+
+    prompt = f"""
+        Based on these ingredients: {ingredients_for_api_call}, provide a recipe suggestions. 
+        Return a list of objects where each object has keys: 
+        "recipe_name", 
+        "prep_time", 
+        "list_of_ingredients" (a list of strings), 
+        "instructions" (a list of steps).
+    """
+
+    response = model.generate_content(prompt)
+    recipes_returned = json.loads(response.text)
+
+    return render(request, "meal_planning/recipes.html",
+                  {'recipes': recipes_returned})
